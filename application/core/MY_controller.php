d4c48549a2c165d6d29cc7161665f0074d22661c
Ayush <ayushsobti@gmail.com>
2012-10-18 02:43:52 -0400

    Added my_model and my_controller to application/core. All application-wide models and controllers extend these, and not CI_Model or CI_Controller

---
+++ application/core/MY_controller.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+	Every controller in our application extends this class
+	which in turn extends CI_Controller
+*/
+class MY_controller extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file MY_controller.php */
+/* Location: ./application/core/MY_controller.php */
