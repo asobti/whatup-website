29aeaa6568fcd10ad75f3b1bb39ee74a03fcac96
Project Whatup <projectwhatup@gmail.com>
2012-10-27 07:18:59 +0000

    renamed core controller base classes

---
+++ application/core/MY_Model.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+	Every model in our application extends this class,
+	which in turn extends CI_Model
+*/
+
+class MY_model extends CI_Model {
+
+	
+
+}
+
+/* End of file MY_model.php */
+/* Location: ./application/core/MY_model.php */
