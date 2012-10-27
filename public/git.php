646177f7428d6da0d14814b2b3d94603e29751ff
Project Whatup <projectwhatup@gmail.com>
2012-10-27 04:55:56 +0000

    added security precautions and logging to git.php

--- public/git.php
+++ public/git.php
@@ -1,5 +1,14 @@
 <?php
 
+// log file
+$log_file = "deploy.log";
+
+// stores the result of the deployment
+$deploy_result = date("m/d/Y H:i:s") . ': ';
+
+// incoming request server's IP
+$request_from = $_SERVER['REMOTE_ADDR'];
+
 // only honor requests when they come from one of these ips
 $github_pub_ips = array(
 	'207.97.227.253',
@@ -7,9 +16,23 @@ $github_pub_ips = array(
 	'108.171.174.178'
 );
 
-if (in_array($_SERVER['REMOTE_ADDR'], $github_pub_ips) {
+if (in_array($request_from, $github_pub_ips)) {
 	if ($_POST['payload']) {
 		// do a git pull
-		`git pull origin master`;
+		if (shell_exec('git pull origin master') !== NULL) {
+			$deploy_result .= "Deployed master successfully.\n";
+		} else {
+			$deploy_result .= "Shell exec failed.\n";
+		}
+	} else {
+		$deploy_result .= "Request from {$request_from} not carrying POST payload.\n";
 	}
+} else {
+	$deploy_result .= "Unauthorized request from {$request_from}.\n";
 }
+
+// store the result to the log file. Log file wii be
+// created if it does not exist, or appended to if it
+// does
+
+file_put_contents($log_file, $deploy_result, FILE_APPEND);
