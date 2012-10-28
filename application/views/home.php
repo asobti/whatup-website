04d3920df3d367113b09d163da5d047610b77159
Ayush <ayushsobti@gmail.com>
2012-10-28 16:38:58 -0400

    rolling back previous change

--- application/views/home.php
+++ application/views/home.php
@@ -9,14 +9,14 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="<?= base_url('assets/css/bootstrap.css') ?>" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href="<?= base_url('assets/css/bootstrap-responsive.css') ?>" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -24,11 +24,11 @@
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="<?= base_url('assets/ico/favicon.ico') ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url('assets/ico/apple-touch-icon-144-precomposed.png') ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url('assets/ico/apple-touch-icon-114-precomposed.png') ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url('assets/ico/apple-touch-icon-72-precomposed.png') ?>">
-    <link rel="apple-touch-icon-precomposed" href="<?= base_url('assets/ico/apple-touch-icon-57-precomposed.png') ?>">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
   </head>
 
   <body>
@@ -113,19 +113,19 @@ Ut vitae metus sit amet metus accumsan dapibus non in enim. Nulla vitae mauris n
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?= base_url('assets/js/jquery.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-transition.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-alert.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-modal.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-dropdown.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-scrollspy.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-tab.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-tooltip.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-popover.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-button.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-collapse.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-carousel.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap-typeahead.js') ?>"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
 
   </body>
 </html>
