791986c315f91d10dd31dbf3e3e0fe123344cfbc
Anthony Hurst <Anthony.Hurst@gmail.com>
2012-11-12 00:33:34 -0500

    Update public/git.php

--- public/git.php
+++ public/git.php
@@ -12,31 +12,32 @@ $request_from = $_SERVER['REMOTE_ADDR'];
 
 // sends emails on failure to the following recipients
 $report_to = array(
-		'ayushsobti@gmail.com',
-		'anthony.hurst@gmail.com'
-	);
+                'ayushsobti@gmail.com',
+                'anthony.hurst@gmail.com'
+        );
 
 // only honor requests when they come from one of these ips
 $github_pub_ips = array(
-	'207.97.227.253',
-	'50.57.128.197',
-	'108.171.174.178'
+        '207.97.227.253',
+        '50.57.128.197',
+        '108.171.174.178'
 );
 
+$branch =  "arena";
 if (in_array($request_from, $github_pub_ips)) {
-	if ($_POST['payload']) {
-		// do a git pull
-		if (shell_exec('git pull origin master') !== NULL) {
-			$deploy_result .= "Deployed master successfully.\n";
-			$deploy_success = true;
-		} else {
-			$deploy_result .= "Shell exec failed.\n";
-		}
-	} else {
-		$deploy_result .= "Request from {$request_from} not carrying POST payload.\n";
-	}
+        if ($_POST['payload']) {
+                // do a git pull
+                if (shell_exec("git pull origin $branch") !== NULL) {
+                        $deploy_result .= "Deployed $branch successfully.\n";
+                        $deploy_success = true;
+                } else {
+                        $deploy_result .= "Shell exec failed.\n";
+                }
+        } else {
+                $deploy_result .= "Request from {$request_from} not carrying POST payload.\n";
+        }
 } else {
-	$deploy_result .= "Unauthorized request from {$request_from}.\n";
+        $deploy_result .= "Unauthorized request from {$request_from}.\n";
 }
 
 // store the result to the log file. Log file wii be
@@ -47,6 +48,8 @@ file_put_contents($log_file, $deploy_result, FILE_APPEND);
 
 // if there was a failure, send email
 if (!$deploy_success) {
-	$to = implode(',', $report_to);
-	mail($to, "ProjectWhatUp Deployment Failure", $deploy_result);
+        $to = implode(',', $report_to);
+        mail($to, "ProjectWhatUp Deployment Failure", $deploy_result);
 }
+
+?>
