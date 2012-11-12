7aa0d07b26c1d714572452eb7a4426f607ad1826
anthonyhurst <Anthony.Hurst@gmail.com>
2012-11-11 19:36:15 -0500

    Broke Views Up into Sections

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,7 +4,9 @@ class Home extends MY_Controller {
 
 	public function index()
 	{
+		$this->load->view("header");
 		$this->load->view("home");
+		$this->load->view("footer");
 	}
 
 }
---
+++ application/views/footer.php
@@ -0,0 +1,30 @@
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+<script type="text/javascript" src="assets/js/json2.js"></script>
+<script type="text/javascript" src="assets/js/underscore.js"></script>
+<script type="text/javascript" src="assets/js/backbone.js"></script>
+<script type="text/javascript" src="assets/js/models/post.js"></script>
+<script type="text/javascript" src="assets/js/views/post.js"></script>
+<script type="text/javascript" src="assets/js/routes/app.js"></script>
+
+  </body>
+</html>
+
