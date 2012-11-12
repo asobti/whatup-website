0712bc335743e3a5a6f990991470039b514e8ba6
Ayush <ayushsobti@gmail.com>
2012-11-11 23:15:54 -0500

    changed urls for assets to absolute

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,19 +13,19 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="<?= base_url('assets/js/jquery.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-transition.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-alert.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-modal.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-dropdown.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-scrollspy.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-tab.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-tooltip.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-popover.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-button.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-collapse.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-carousel.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-typeahead.js') ?>"></script>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
--- application/views/header.php
+++ application/views/header.php
@@ -8,14 +8,14 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="<?= base_url('assets/css/bootstrap.css') ?>" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?= base_url('assets/css/bootstrap-responsive.css') ?>" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -23,11 +23,11 @@
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?= base_url('assets/ico/favicon.ico') ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url('assets/ico/apple-touch-icon-144-precomposed.png') ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url('assets/ico/apple-touch-icon-114-precomposed.png') ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url('assets/ico/apple-touch-icon-72-precomposed.png') ?>">
+    <link rel="apple-touch-icon-precomposed" href="<?= base_url('assets/ico/apple-touch-icon-57-precomposed.png') ?>">
   </head>
 
   <body>
