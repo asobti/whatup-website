7cd23b87b2faa6e83158a33e1695a253bc424827
Project Whatup <projectwhatup@gmail.com>
2013-01-18 04:33:10 +0000

    Added Tag Specific Searching

--- public/assets/js/controllers/PostsCtrl.js
+++ public/assets/js/controllers/PostsCtrl.js
@@ -27,19 +27,52 @@ function PostsCtrl($scope, $http, $location, $routeParams, eventBus, Posts) {
 					}
 				]
 			};
+			
+			var advancedSearchObj = getAdvancedSearch($scope.searchData);
 
 			if(typeof $scope.searchData !== 'undefined' && $scope.searchData !== "") {
 				// a non-empty search term was defined
 				// create a filter and add it to the queryObj
 				var filter = [
 								{
-									name : "body",
-									op : "like",
-									val : "%" + $scope.searchData + "%"
+									name : advancedSearchObj.name,
+									op : advancedSearchObj.op,
+									val : advancedSearchObj.val
 								}
 							];
 
 				queryObj.filters = filter;
+			}
+			
+
+			function getAdvancedSearch(searchData) {
+				//need to clean this up a bit to be scalable.
+				
+				//defaults
+				var name = "body";
+				var op = "like";
+				var val = searchData;
+
+				//check for tag
+				var tagPattern = /tag:(.*)/g;
+				var tagMatch = tagPattern.exec(searchData);
+				if (tagMatch  !== null) {
+					name = "tags__name";
+					op = "any";
+					val = tagMatch[1];
+				}
+	
+				if (op == "like") {
+					val = "%" + val + "%";  //other operators don't like the %
+				}
+				
+				var search = {
+					"name" : name,
+					"op" : op,
+					"val" : val
+				};
+				console.log(search);
+				return search;
 			}
 
 			Posts.query({
